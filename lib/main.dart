d2e5c166c3a0f93482a306b7260713dfa8511ff3
leofab <leofabr3@gmail.com>
2025-01-24 15:16:07 -0300

    firebase_core with correct google-services

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './screens/onboarding/onboarding_screen.dart';
 import 'package:app/screens/auth/login_screen.dart';
@@ -5,6 +6,7 @@ import 'utils/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     const MainApp(),
   );
